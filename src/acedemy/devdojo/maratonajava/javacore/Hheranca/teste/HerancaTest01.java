e6dd55f80d07cb1c518986ad835da82b457199c5
Felipe Taleires <76842760+felipe-st@users.noreply.github.com>
2024-06-11 21:54:27 -0300

    até herança (protected)

---
+++ src/acedemy/devdojo/maratonajava/javacore/Hheranca/teste/HerancaTest01.java
@@ -0,0 +1,26 @@
+package acedemy.devdojo.maratonajava.javacore.Hheranca.teste;
+
+import acedemy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import acedemy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import acedemy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("012345-209");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Toyohisa Shimazu");
+        pessoa.setCpf("111111");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Oda Nobunaga");
+        funcionario.setCpf("2222");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+        System.out.println("------------------");
+        funcionario.imprime();
+    }
+}
